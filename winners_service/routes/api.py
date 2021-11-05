ea3f70b785cd1512254d27325838b92e0f836bce
Landon Newberry <landonnewberry@gmail.com>
2021-11-05 11:20:18 -0700

    new message

--- winners_service/routes/api.py
+++ winners_service/routes/api.py
@@ -16,4 +16,4 @@ def get_status():
     description="Get root."
 )
 def get_status():
-    return PlainTextResponse(status_code=200, content="OK")
+    return PlainTextResponse(status_code=200, content="We are the winners")
